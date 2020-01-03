225237d8dd4732a8ec34ae7e648017960e233d4d
Geoff <geoffhome@mbp-de-geoff.home>
2020-01-04 00:19:05 +0100

    initial commit swing

---
+++ swing/src/MainFrame.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class MainFrame extends JFrame {
+
+    private static final long serialVersionUID = 1L;
+    
+    public static void main(String argv[]) {
+        JFrame frame = new MainFrame();
+
+    }
+
+}
